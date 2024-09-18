c0730f3651bb4311326a33601937091dd3f91956
Marcus0086 <guptamarcus42@gmail.com>
2024-09-18 15:22:40 +0530

    Config loaders

---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"Burst/pkg/models"
+	"os"
+	"path/filepath"
+
+	"github.com/hashicorp/hcl"
+)
+
+func LoadConfig(filename string) (*models.Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config models.Config
+	err = hcl.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func LoadConfigs(path string) ([]*models.Config, error) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var configs []*models.Config
+
+	for _, file := range files {
+		if file.IsDir() || filepath.Ext(file.Name()) != ".hcl" {
+			continue
+		}
+		config, err := LoadConfig(filepath.Join(path, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		configs = append(configs, config)
+	}
+
+	return configs, nil
+}
