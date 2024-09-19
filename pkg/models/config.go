d01ca995801c1a21354b91622b2d1b07c7d2d000
Marcus0086 <guptamarcus42@gmail.com>
2024-09-20 01:08:32 +0530

    Refactor config loading functions and add LoadRootConfig

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -1,8 +1,9 @@
 package models
 
 type Config struct {
-	Global GlobalConfig `hcl:"global"`
-	Server ServerConfig `hcl:"server"`
+	Global  GlobalConfig `hcl:"global"`
+	Server  ServerConfig `hcl:"server"`
+	Include []string     `hcl:"include"`
 }
 
 type GlobalConfig struct {
@@ -19,11 +20,15 @@ type ServerConfig struct {
 }
 
 type RouteConfig struct {
-	Path       string   `hcl:"path"`
-	Handler    string   `hcl:"handler"`
-	Script     string   `hcl:"script"`
-	Template   string   `hcl:"template"`
-	Middleware []string `hcl:"middleware"`
+	Path          string                 `hcl:"path"`
+	Handler       string                 `hcl:"handler"`
+	Template      string                 `hcl:"template,optional"`
+	Body          string                 `hcl:"body,optional"`
+	Middleware    []string               `hcl:"middleware,optional"`
+	Data          map[string]interface{} `hcl:"data,optional"`
+	Target        string                 `hcl:"target,optional"`
+	Targets       []string               `hcl:"targets,optional"`
+	LoadBalancing string                 `hcl:"load_balancing,optional"`
 }
 
 type ErrorPageConfig struct {
