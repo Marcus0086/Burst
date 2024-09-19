bebb572116f5c9a24511da9da618c738e362a9da
Marcus0086 <guptamarcus42@gmail.com>
2024-09-20 01:09:45 +0530

    Refactor server startup logic and handle graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,19 +4,21 @@ import (
 	"Burst/internal/handlers"
 	"Burst/internal/utils"
 	"Burst/pkg/models"
+	"context"
 	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"golang.org/x/net/http2"
 )
 
-func StartServer(config *models.Config) {
+func StartServer(ctx context.Context, config *models.Config) {
 
 	if config.Server.Listen == "" {
-		config.Server.Listen = ":80"
+		log.Fatal("Listen address is not set")
 	}
 
 	addr := config.Server.Listen
@@ -27,25 +29,49 @@ func StartServer(config *models.Config) {
 		}),
 	}
 
-	if strings.HasSuffix(addr, ":443") || config.Server.HTTPS {
-		cert, err := utils.GenerateSelfSignedCert()
+	errChan := make(chan error, 1)
+
+	go func() {
+		var err error
+		if strings.HasSuffix(addr, ":443") || config.Server.HTTPS {
+			cert, err := utils.GenerateSelfSignedCert()
+			if err != nil {
+				log.Printf("Error loading certificate and key for %s: %v", addr, err)
+				errChan <- err
+				return
+			}
+			server.TLSConfig = &tls.Config{Certificates: []tls.Certificate{cert}}
+			http2.ConfigureServer(server, &http2.Server{})
+			err = server.ListenAndServeTLS("", "")
+			if err != nil && err != http.ErrServerClosed {
+				log.Printf("Error starting server on %s: %v", addr, err)
+			}
+		} else {
+			err = server.ListenAndServe()
+			if err != nil && err != http.ErrServerClosed {
+				log.Printf("Error starting server on %s: %v", addr, err)
+			}
+			if err != nil && err != http.ErrServerClosed {
+				log.Printf("Error starting server on %s: %v", addr, err)
+				errChan <- err
+
+			}
+			close(errChan)
+		}
+	}()
+	fmt.Println("Server started on", config.Server.Listen)
+	select {
+	case <-ctx.Done():
+		log.Printf("Shutting down server on %s", addr)
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			log.Printf("Server forced to shutdown: %v", err)
+		}
+		log.Printf("Server on %s gracefully shut down", addr)
+	case err := <-errChan:
 		if err != nil {
-			log.Printf("Error loading certificate and key for %s: %v", addr, err)
-			return
-		}
-		server.TLSConfig = &tls.Config{Certificates: []tls.Certificate{cert}}
-		http2.ConfigureServer(server, &http2.Server{})
-		err = server.ListenAndServeTLS("", "")
-		fmt.Println("Server started on", config.Server.Listen)
-		if err != nil && err != http.ErrServerClosed {
-			log.Printf("Error starting server on %s: %v", addr, err)
-		}
-	} else {
-		err := server.ListenAndServe()
-		fmt.Println("Server started on", config.Server.Listen)
-		if err != nil && err != http.ErrServerClosed {
-			log.Printf("Error starting server on %s: %v", addr, err)
+			log.Printf("Server on %s encountered an error: %v", addr, err)
 		}
 	}
-
 }
