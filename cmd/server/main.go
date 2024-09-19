14d9f71ba5a66a2254355cb7e916cdad92921395
Marcus0086 <guptamarcus42@gmail.com>
2024-09-20 01:06:58 +0530

    Refactor server main function to use context and graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,34 +4,54 @@ import (
 	"Burst/internal/config"
 	"Burst/internal/worker"
 	"Burst/pkg/models"
+	"context"
 	"log"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
 const maxWorkers = 10
 
 func main() {
-
+	allConfigs := make([]*models.Config, 0)
+	rootConfig, err := config.LoadRootConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	allConfigs = append(allConfigs, rootConfig)
 	configs, err := config.LoadConfigs("./config")
 	if err != nil {
 		log.Fatal(err)
 	}
-	workers := configs[0].Global.Workers
+	allConfigs = append(allConfigs, configs...)
+	workers := rootConfig.Global.Workers
 	if workers <= 0 {
 		workers = maxWorkers
 	}
 
-	jobs := make(chan *models.Config, workers)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
 	var wg sync.WaitGroup
 
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		go worker.Worker(jobs, &wg)
+		go worker.Worker(ctx, &wg)
 	}
 
-	for _, config := range configs {
-		jobs <- config
-	}
-	close(jobs)
+	go func() {
+		for _, config := range allConfigs {
+			worker.AddJob(config)
+		}
+		worker.CloseJobs()
+	}()
+
+	<-stop
+	cancel()
 	wg.Wait()
 }
