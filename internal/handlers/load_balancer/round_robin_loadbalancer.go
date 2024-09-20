cec19c5d55f5979379bca156e29a393f9b11b5ce
Marcus0086 <guptamarcus42@gmail.com>
2024-09-20 18:15:06 +0530

    Refactor load balancer logic to use Backend struct and handle backend failures

--- internal/handlers/load_balancer/round_robin_loadbalancer.go
+++ internal/handlers/load_balancer/round_robin_loadbalancer.go
@@ -1,21 +1,86 @@
 package loadbalancer
 
 import (
+	"errors"
+	"log"
+	"net"
 	"net/url"
 	"sync"
+	"time"
 )
 
+type Backend struct {
+	URL *url.URL
+	Alive bool
+	mutex sync.RWMutex
+}
+
+func (b *Backend) IsAlive() bool {
+	b.mutex.RLock()
+	defer b.mutex.RUnlock()
+	return b.Alive
+}
+
+func (b *Backend) SetAlive(alive bool) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+	b.Alive = alive
+}
+
+
 type RoundRobin struct {
-	Targets []*url.URL
+	Targets []*Backend
 	mutex   sync.Mutex
 	index   int
 }
 
-func (rr *RoundRobin) NextTarget() *url.URL {
+func (rr *RoundRobin) NextTarget() (*Backend, error) {
 	rr.mutex.Lock()
 	defer rr.mutex.Unlock()
 
-	target := rr.Targets[rr.index%len(rr.Targets)]
-	rr.index++
-	return target
+	totalTargets := len(rr.Targets)
+	startingIndex := rr.index
+
+	for {
+		target := rr.Targets[rr.index%totalTargets]
+		rr.index++
+		if target.IsAlive() {
+			return target, nil
+		}
+		if rr.index%totalTargets == startingIndex%totalTargets {
+			break
+		}
+	}
+	return nil, errors.New("no available targets")
+}
+
+func (rr *RoundRobin) Backends() []*Backend {
+	return rr.Targets
+}
+
+func healthCheck(backends []*Backend, interval time.Duration) {
+	for {
+		var wg sync.WaitGroup
+		for _, backend := range backends {
+			wg.Add(1)
+			go func(backend *Backend) {
+				defer wg.Done()
+				alive := isBackendAlive(backend.URL)
+				backend.SetAlive(alive)
+			}(backend)
+		}
+		wg.Wait()
+		time.Sleep(interval)
+	}
+}
+
+func isBackendAlive(target *url.URL) bool {
+	timeout := time.Duration(2 * time.Second)
+	conn, err := net.DialTimeout("tcp", target.Host, timeout)
+	if err != nil {
+		log.Println("Error:", err)
+		return false
+	}
+	defer conn.Close()
+	return true
 }
