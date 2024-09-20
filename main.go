4251d74a4b298e2a1896da7cfee0b8f853b901f2
Marcus0086 <guptamarcus42@gmail.com>
2024-09-21 00:20:33 +0530

    Refactor main.go to use Burst package for worker configuration and execution

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"Burst/internal/config"
+	"Burst/internal/worker"
+	"Burst/pkg/models"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+const maxWorkers = 10
+
+func main() {
+	allConfigs := make([]*models.Config, 0)
+	rootConfig, err := config.LoadRootConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	allConfigs = append(allConfigs, rootConfig)
+	if _, err := os.Stat("./config"); !os.IsNotExist(err) {
+		configs, err := config.LoadConfigs("./config")
+		if err != nil {
+			log.Default().Println(err)
+		} else {
+			allConfigs = append(allConfigs, configs...)
+		}
+	}
+	workers := rootConfig.Global.Workers
+	if workers <= 0 {
+		workers = maxWorkers
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go worker.Worker(ctx, &wg)
+	}
+
+	go func() {
+		for _, config := range allConfigs {
+			worker.AddJob(config)
+		}
+		worker.CloseJobs()
+	}()
+
+	<-stop
+	cancel()
+	wg.Wait()
+}
