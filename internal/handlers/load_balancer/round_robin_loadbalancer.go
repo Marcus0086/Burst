6fe30508e6adfc9e72d366111a4ea8279bcaa27a
Marcus0086 <guptamarcus42@gmail.com>
2024-09-21 00:21:18 +0530

    Refactor round_robin_loadbalancer.go to remove unused code and dependencies, and optimize health check implementation

--- internal/handlers/load_balancer/round_robin_loadbalancer.go
+++ internal/handlers/load_balancer/round_robin_loadbalancer.go
@@ -2,8 +2,6 @@ package loadbalancer
 
 import (
 	"errors"
-	"log"
-	"net"
 	"net/url"
 	"sync"
 	"time"
@@ -58,29 +56,9 @@ func (rr *RoundRobin) Backends() []*Backend {
 	return rr.Targets
 }
 
-func healthCheck(backends []*Backend, interval time.Duration) {
-	for {
-		var wg sync.WaitGroup
-		for _, backend := range backends {
-			wg.Add(1)
-			go func(backend *Backend) {
-				defer wg.Done()
-				alive := isBackendAlive(backend.URL)
-				backend.SetAlive(alive)
-			}(backend)
-		}
-		wg.Wait()
-		time.Sleep(interval)
-	}
-}
-
-func isBackendAlive(target *url.URL) bool {
-	timeout := time.Duration(2 * time.Second)
-	conn, err := net.DialTimeout("tcp", target.Host, timeout)
-	if err != nil {
-		log.Println("Error:", err)
-		return false
-	}
-	defer conn.Close()
-	return true
+func roundRobin(targets []*Backend) (LoadBalancer, error) {
+	lb := &RoundRobin{Targets: targets}
+	performInitialHealthCheck(targets)
+	go healthCheck(targets, 10*time.Second)
+	return lb, nil
 }
