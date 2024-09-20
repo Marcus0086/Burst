cd4a66e70e0b30c566c2901b872877456b97025b
Marcus0086 <guptamarcus42@gmail.com>
2024-09-20 18:14:58 +0530

    Refactor load balancer creation logic and add support for caching instances

--- internal/handlers/load_balancer/load_balancer.go
+++ internal/handlers/load_balancer/load_balancer.go
@@ -4,23 +4,67 @@ import (
 	"Burst/pkg/models"
 	"fmt"
 	"net/url"
+	"sync"
+	"time"
 )
 
-func CreateLoadBalancer(route *models.RouteConfig) (models.LoadBalancer, error) {
-	var targets []*url.URL
+type LoadBalancer interface {
+	NextTarget() (*Backend, error)
+	Backends() []*Backend
+}
 
+func CreateLoadBalancer(route *models.RouteConfig) (LoadBalancer, error) {
+	var targets []*Backend
+	if (route.Target != "") {
+		u, err := url.Parse(route.Target)
+		if err != nil {
+			return nil, fmt.Errorf("invalid target URL %s: %v", route.Target, err)
+		}
+		backend := &Backend{URL: u, Alive: false}
+		targets = append(targets, backend)
+	}
 	for _, target := range route.Targets {
 		u, err := url.Parse(target)
 		if err != nil {
 			return nil, fmt.Errorf("invalid target URL %s: %v", target, err)
 		}
-		targets = append(targets, u)
+		backend := &Backend{URL: u, Alive: false}
+		targets = append(targets, backend)
 	}
 
 	switch route.LoadBalancing {
 	case "round_robin":
-		return &RoundRobin{Targets: targets}, nil
+		lb, err := roundRobin(targets)
+		if err != nil {
+			return nil, err
+		}
+		return lb, nil
 	default:
-		return nil, fmt.Errorf("unknown load balancing method %s", route.LoadBalancing)
+		lb, err := roundRobin(targets)
+		if err != nil {
+			return nil, err
+		}
+		return lb, nil
 	}
+}
+
+func roundRobin(targets []*Backend) (LoadBalancer, error) {
+	lb := &RoundRobin{Targets: targets}
+	performInitialHealthCheck(targets)
+	go healthCheck(targets, 10*time.Second)
+	return lb, nil
+}
+
+
+func performInitialHealthCheck(backends []*Backend) {
+    var wg sync.WaitGroup
+    for _, b := range backends {
+        wg.Add(1)
+        go func(b *Backend) {
+            defer wg.Done()
+            alive := isBackendAlive(b.URL)
+            b.SetAlive(alive)
+        }(b)
+    }
+    wg.Wait()
 }
